0ed19c977e1a7edbba7d4c68d6b825c930464db1
Pablo Winck Winter <70986781+pablowinck@users.noreply.github.com>
2022-07-28 16:28:54 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="table">
+      <input placeholder="nome da empresa" className="item" />
+      <input placeholder="nome" className="item" />
+      <input placeholder="cnpj" className="item" />
+      <input placeholder="email" className="item" />
+      <input placeholder="telefone" className="item" />
+    </div>
+  );
+}
